b14a4e8049524c0619034d35272fbaac36e5be4f
maheenriaz <mahiriaz99@gmail.com>
2020-05-09 08:54:34 -0700

    Updates

---
+++ precache-manifest.d14f4835f16034046ac5b984c769c091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47ad52f3d6d62c2042268450233fef7",
+    "url": "/COVID-19-LATEST-UPDATES/index.html"
+  },
+  {
+    "revision": "2e29d55412a6858e0dcb",
+    "url": "/COVID-19-LATEST-UPDATES/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ae64f4fdff685e0eb3c6",
+    "url": "/COVID-19-LATEST-UPDATES/static/css/main.9b77730a.chunk.css"
+  },
+  {
+    "revision": "2e29d55412a6858e0dcb",
+    "url": "/COVID-19-LATEST-UPDATES/static/js/2.6d579912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/COVID-19-LATEST-UPDATES/static/js/2.6d579912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae64f4fdff685e0eb3c6",
+    "url": "/COVID-19-LATEST-UPDATES/static/js/main.e6367cce.chunk.js"
+  },
+  {
+    "revision": "7118a2ea64beef4f45c2",
+    "url": "/COVID-19-LATEST-UPDATES/static/js/runtime-main.a469c236.js"
+  }
+]);
